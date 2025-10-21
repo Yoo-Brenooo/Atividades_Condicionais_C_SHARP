a053027211174b6a97aaf7f0e6f969bedfd1f641
Breno <brenooliveirasouza13@gmail.com>
2025-10-20 21:58:56 -0300

    Add files via upload

---
+++ Calculadora/Program.cs
@@ -0,0 +1,47 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Calculadora
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                string operador;
+                int x, y;
+                int resultado = 0;
+
+                Console.Write("Digite o primeiro numero: ");
+                x = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o primeiro numero: ");
+                y = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o operador (+, _, *, /): ");
+                operador = Console.ReadLine();
+
+                switch (operador) {
+                    case "+":
+                        resultado = x + y;
+                        break;
+                    case "-":
+                        resultado = x - y;
+                        break;
+                    case "*":
+                        resultado = x * y;
+                        break;
+                    case "/":
+                        resultado = x / y;
+                        break;
+                    default:
+                        Console.WriteLine("Opção invalida");
+                        break;
+                }
+
+                Console.WriteLine($"O resultado é: {resultado}");
+            }
+        }
+    }
