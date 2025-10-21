974e947d1e130a030a33bb3231a7f7e1fd1a654d
Breno <brenooliveirasouza13@gmail.com>
2025-10-20 21:28:22 -0300

    Add files via upload

---
+++ Cores/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Cores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cor_ingles;
+
+            Console.Write("Digite uma cor em ingles (vermelho, verde, azul ou roxo): ");
+            cor_ingles = Console.ReadLine(). ToUpper();
+
+            switch (cor_ingles) {
+
+                case "RED":
+                    Console.WriteLine("Vermelho");
+                    break;
+                case "GREEN":
+                    Console.WriteLine("Verde");
+                    break;
+                case "BLUE":
+                    Console.WriteLine("Azul");
+                    break;
+                case "PURPLE":
+                    Console.WriteLine("Roxo");
+                    break;
+                default:
+                    Console.WriteLine("Opção invalida");
+                    break;
+            }
+        }
+    }
+}
---
+++ Cores/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Cores")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cores")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("e236019b-7153-4e65-ab7c-a5d886c58dd9")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
